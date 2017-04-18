03d714ac06bc2f8f032bd71e2e36e65500f2342e
pc37utn <pc37@utk.edu>
2017-04-18 13:16:33 -0400

    remove adminmal

--- scripts/custom_scripts/070_drush_add_theme.sh
+++ scripts/custom_scripts/070_drush_add_theme.sh
@@ -25,28 +25,28 @@ drush eval "variable_set('theme_default', 'UTKdrupal')"
 
 # Admin Theme Adminimal https://www.drupal.org/project/adminimal_theme
 cd "$DRUPAL_HOME"/sites/all/modules/ || exit
-drush dl adminimal_theme adminimal_admin_menu module_filter
-drush en -y adminimal adminimal_admin_menu module_filter
+#drush dl adminimal_theme adminimal_admin_menu module_filter
+#drush en -y adminimal adminimal_admin_menu module_filter
 
-drush -y vset admin_theme adminimal
-drush -y vset admin_menu_margin_top 0
-drush -y vset adminimal_admin_menu_render "hidden"
+#drush -y vset admin_theme adminimal
+#drush -y vset admin_menu_margin_top 0
+#drush -y vset adminimal_admin_menu_render "hidden"
 
 # drush dis -y comment contextual dashboard overlay
 
 # Disable Unneeded Themes
-drush -y dis bartik
+#drush -y dis bartik
 #drush -y dis seven
 
 # Enable devel and its prereqs
 drush -y vset theme_debug 1
 drush -y dis devel
 drush -y dl devel
-sudo apt-get -y install subversion
+sudo yum -y install subversion
 drush -y en devel
 
 # Rebuild on every page, needs to be disabled on production env
 devel_rebuild_theme_registry: 1
 
 # Fix Admin menu
-drush -y vset admin_menu_margin_top 1
+#drush -y vset admin_menu_margin_top 1
