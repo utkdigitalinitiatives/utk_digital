24a34f14a61d72b7af78b111fd2a6c32a6fa74a8
pc37utn <pc37@utk.edu>
2017-04-18 13:44:09 -0400

    cleanup scholar,webarchive,disk iso removal

--- scripts/islandora_modules.sh
+++ scripts/islandora_modules.sh
@@ -30,6 +30,7 @@ while read -r LINE; do
   git config core.filemode false
   cd "$DRUPAL_HOME"/sites/all/modules || exit
 done < "$SHARED_DIR"/configs/islandora-module-list-sans-tuque.txt
+git clone git://github.com/Islandora-Labs/islandora_binary_object
 
 # Clone Tuque, BagItPHP, and Cite-Proc
 cd "$DRUPAL_HOME"/sites/all || exit
@@ -41,7 +42,6 @@ git clone https://github.com/Islandora/tuque.git
 git clone git://github.com/scholarslab/BagItPHP.git
 #git clone https://github.com/Islandora/citeproc-php.git
 
-git clone git://github.com/Islandora-Labs/islandora_binary_object
 
 cd "$DRUPAL_HOME"/sites/all/libraries/tuque || exit
 git config core.filemode false
@@ -81,22 +81,17 @@ drush -y -u 1 en islandora_solr_facet_pages
 drush -y -u 1 en islandora_solr_views
 drush -y -u 1 en islandora_basic_collection islandora_pdf islandora_audio
 drush -y -u 1 en islandora_book islandora_compound_object
-drush -y -u 1 en islandora_disk_image
-drush -y -u 1 en islandora_entities islandora_entities_csv_import
 drush -y -u 1 en islandora_basic_image islandora_large_image
-drush -y -u 1 en islandora_newspaper islandora_video islandora_web_archive
+drush -y -u 1 en islandora_newspaper islandora_video
 drush -y -u 1 en islandora_premis islandora_checksum islandora_checksum_checker
 drush -y -u 1 en islandora_book_batch
 drush -y -u 1 en islandora_pathauto
 drush -y -u 1 en islandora_pdfjs islandora_videojs islandora_jwplayer
 drush -y -u 1 en xml_forms xml_form_builder xml_schema_api xml_form_elements
 drush -y -u 1 en xml_form_api jquery_update zip_importer islandora_basic_image
-#drush -y -u 1 en islandora_bibliography
 drush -y -u 1 en islandora_compound_object
 drush -y -u 1 en islandora_google_scholar
-#drush -y -u 1 en islandora_scholar_embargo
 drush -y -u 1 en islandora_solr_config
-#drush -y -u 1 en citation_exporter doi_importer endnotexml_importer pmid_importer ris_importer
 drush -y -u 1 en islandora_fits islandora_ocr islandora_oai
 drush -y -u 1 en islandora_simple_workflow
 drush -y -u 1 en islandora_xacml_api islandora_xacml_editor
@@ -105,7 +100,6 @@ drush -y -u 1 en islandora_internet_archive_bookreader
 #drush -y -u 1 en islandora_bagit islandora_batch_report
 drush -y -u 1 en islandora_usage_stats
 drush -y -u 1 en islandora_form_fieldpanel
-#drush -y -u 1 en islandora_populator
 drush -y -u 1 en islandora_newspaper_batch
 
 drush -y -u 1 en islandora_binary_object
