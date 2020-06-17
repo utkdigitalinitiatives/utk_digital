0e714fec06bcfb1925d882c18fb528eb912ade39
pc37utn <pc37@utk.edu>
2020-06-17 12:04:39 -0400

    change back to json base and comment all module loading

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Setup a user for Tomcat Manager
-sed -i '$i<role rolename="admin-gui"/>' /etc/tomcat/tomcat-users.xml
-sed -i '$i<role rolename="manager-gui"/>' /etc/tomcat/tomcat-users.xml
-sed -i '$i<user username="islandora" password="islandora" roles="manager-gui,admin-gui"/>' /etc/tomcat/tomcat-users.xml
+#sed -i '$i<role rolename="admin-gui"/>' /etc/tomcat/tomcat-users.xml
+#sed -i '$i<role rolename="manager-gui"/>' /etc/tomcat/tomcat-users.xml
+#sed -i '$i<user username="islandora" password="islandora" roles="manager-gui,admin-gui"/>' /etc/tomcat/tomcat-users.xml
 systemctl restart tomcat
 
 # add a redirect to collections from web root ( until we get something else there)
