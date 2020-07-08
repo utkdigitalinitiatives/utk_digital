9af561288cc36d0f8ad4824123dcfc815cbbd6b9
pc37utn <pc37utn@users.noreply.github.com>
2020-07-08 11:06:56 -0400

    remove commented lines

--- scripts/islandora_modules.sh
+++ scripts/islandora_modules.sh
@@ -30,41 +30,12 @@ sudo chmod -R 755 "$DRUPAL_HOME"/sites/all/modules/custom
 
 
 cd "$DRUPAL_HOME"/sites/all/modules || exit
-# Clone all non-Islandora modules
-# cd "$DRUPAL_HOME"/sites/all/modules || exit
-#- islandora_bagit -- custom
-# git clone https://github.com/utkdigitalinitiatives/islandora_bagit
-# - utk_lib_feedback
-# git clone https://github.com/utkdigitalinitiatives/utk_lib_feedback
-#- islandora_datastream_replace
-# git clone https://github.com/pc37utn/islandora_datastream_replace
-#git config core.filemode false
 
 # Check for a user's .drush folder, create if it doesn't exist
 if [ ! -d "$HOME_DIR/.drush" ]; then
   mkdir "$HOME_DIR/.drush"
   sudo chown vagrant:vagrant "$HOME_DIR"/.drush
 fi
-
-# Move OpenSeadragon drush file to user's .drush folder
-#if [ -d "$HOME_DIR/.drush" ] && [ -f "$DRUPAL_HOME/sites/all/modules/islandora_openseadragon/islandora_openseadragon.drush.inc" ]; then
-#  mv "$DRUPAL_HOME/sites/all/modules/islandora_openseadragon/islandora_openseadragon.drush.inc" "$HOME_DIR/.drush"
-#fi
-
-# Move video.js drush file to user's .drush folder
-#if [ -d "$HOME_DIR/.drush" ] && [ -f "$DRUPAL_HOME/sites/all/modules/islandora_videojs/islandora_videojs.drush.inc" ]; then
-#  mv "$DRUPAL_HOME/sites/all/modules/islandora_videojs/islandora_videojs.drush.inc" "$HOME_DIR/.drush"
-#fi
-
-# Move pdf.js drush file to user's .drush folder
-#if [ -d "$HOME_DIR/.drush" ] && [ -f "$DRUPAL_HOME/sites/all/modules/islandora_pdfjs/islandora_pdfjs.drush.inc" ]; then
-#  mv "$DRUPAL_HOME/sites/all/modules/islandora_pdfjs/islandora_pdfjs.drush.inc" "$HOME_DIR/.drush"
-#fi
-
-# Move IA Bookreader drush file to user's .drush folder
-#if [ -d "$HOME_DIR/.drush" ] && [ -f "$DRUPAL_HOME/sites/all/modules/islandora_internet_archive_bookreader/islandora_internet_archive_bookreader.drush.inc" ]; then
-#  mv "$DRUPAL_HOME/sites/all/modules/islandora_internet_archive_bookreader/islandora_internet_archive_bookreader.drush.inc" "$HOME_DIR/.drush"
-#fi
 
 # Enable Modules
 drush -y -u 1 en libraries 
