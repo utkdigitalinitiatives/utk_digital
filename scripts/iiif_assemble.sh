7f11517c51f5f4e6bbc97eb685c154e488c8a9ff
Mat Jordan <mat@utk.edu>
2021-02-16 23:58:32 -0500

    Add composer install, some ignores.

--- scripts/iiif_assemble.sh
+++ scripts/iiif_assemble.sh
@@ -18,7 +18,9 @@ cd assemble
 sudo git remote remove origin
 sudo git remote add origin git@github.com:mathewjordan/iiif_assemble.git
 
-# echo "Installing Composer"
-# php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-# php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-# sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+echo "Installing Composer"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php /usr/local/bin/composer install
